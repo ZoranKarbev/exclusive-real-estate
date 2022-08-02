7f1f5895a5cb03e608d9f515651396b9220e6ac5
ZoranK <zorankarbev@gmail.com>
2022-08-02 05:33:54 +0200

    added firebase sign up functionality

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { db } from "../../firebase.config"
 import { ReactComponent as ArrowRightIcon } from "../../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../../assets/svg/visibilityIcon.svg"
 
@@ -18,12 +20,26 @@ const SignUpPage = () => {
       [e.target.id]: e.target.value
     }))
   }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredentials.user;
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <>
       <div className="page__container">
         <h2 className="page__header">Welcome Back!</h2>
         <main>
-          <form className="signup-form">
+          <form className="signup-form" onSubmit={handleSubmit}>
             <input type="text" className="name-input" placeholder="Name" id="name" value={name} onChange={handleChange} />
             <input type="email" className="email-input" placeholder="Email" id="email" value={email} onChange={handleChange} />
             <div className="password-input-div">
