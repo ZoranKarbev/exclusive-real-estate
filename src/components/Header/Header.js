a2cbbd1dca184c968948fcec47517a73369d0478
ZoranK <zorankarbev@gmail.com>
2022-08-02 04:48:58 +0200

    fixed header width

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 
 const Header = () => {
     return (
-        <div><h1 className="header__title">Exclusive Real Estate</h1></div>
+        <div className="header"><h1 className="header__title">Exclusive Real Estate</h1></div>
     )
 };
 
